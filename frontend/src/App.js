4647e43d47cdac404816680d32ca7918351e5bad
hussein <husseinaegh9@gmail.com>
2025-06-16 19:11:45 +0300

    1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Welcome to Budgeting App</h1>
-        <p>Track your budget efficiently and effortlessly.</p>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
